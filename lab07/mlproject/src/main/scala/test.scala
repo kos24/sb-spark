1391a6bd909c3f1f23813ebf10cadab804d3110e
Konstantin <rebrin24@gmail.com>
2021-04-12 15:21:10 +0500

    changed trigger condition

--- lab07/mlproject/src/main/scala/test.scala
+++ lab07/mlproject/src/main/scala/test.scala
@@ -74,8 +74,8 @@ object test extends App {
   val writer = result
     .writeStream
     .format("kafka")
-    //     .trigger(Trigger.Once())
-    .trigger(Trigger.ProcessingTime("10 seconds"))
+         .trigger(Trigger.Once())
+//    .trigger(Trigger.ProcessingTime("10 seconds"))
     .option("kafka.bootstrap.servers", "spark-master-1:6667")
     .option("topic", "konstantin_rebrin_lab07_out")
     .option("checkpointLocation", "/user/konstantin.rebrin/chk/konstantin_rebrin_lab07")
